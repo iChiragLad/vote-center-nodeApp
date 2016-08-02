bf1e74d8a61d01cb7a75ef6b815570884cf5d555
unknown <chirag_lad@PTD09704.persistent.co.in>
2016-08-02 16:49:56 +0530

    Login and Sign up created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/../public/html/home.html'));
+  res.render('pages/index', {'title' : 'VoteCenter - Get opinions instantly'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('pages/login', {'title' : 'VoteCenter - Login'});
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('pages/signup', {'title' : 'VoteCenter - Sign up'});
 });
 
 module.exports = router;
