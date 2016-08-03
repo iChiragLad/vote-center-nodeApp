ecaa6425dbfd8ccbc1470161d8751e0eb832fc32
unknown <chirag_lad@PTD09704.persistent.co.in>
2016-08-03 18:33:50 +0530

    dashboard created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+router.use(express.static(path.join(__dirname,'/../', 'public')));
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+router.post('/dashboard', function(req, res, next){
+  console.log(req.body);
+  res.render('pages/dashboard', {'title':'VoteCenter - Dashboard'});
 });
 
 module.exports = router;
