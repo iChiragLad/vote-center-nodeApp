01526922ff2ccc8645c47f19fcc783ce197baac3
Chirag Lad <chiragladn92@gmail.com>
2016-08-14 12:27:44 +0530

    fix4 - facebook auth

--- configuration/config.js
+++ configuration/config.js
@@ -5,8 +5,8 @@ module.exports = {
     secret : '12345-67891-23456-78912',
     mongoUrl : "mongodb://chirag:admin@ds031835.mlab.com:31835/vote_test",
     facebookAuth : {
-        clientID      : '1762753370661281', // App ID
-        clientSecret  : 'b5a8af8779471624e15176b716a3c867', // App Secret
+        clientID      : '1790826791129210', // App ID
+        clientSecret  : '8be8a8b825367deaab4b01890e4d0454', // App Secret
         callbackURL   : 'https://vote-center.herokuapp.com/users/facebook/callback'
     },
 };
